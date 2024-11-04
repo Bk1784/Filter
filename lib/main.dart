14f3d365a3d5ce14457c6333f0d432dd882e1f23
bk1784 <futonabuu456@gmail.com>
2024-11-04 12:39:24 +0700

    filter

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_latihan_2/widget/filter_carousel.dart';
+
+void main() { 
+runApp( 
+const MaterialApp( 
+home: PhotoFilterCarousel(), 
+debugShowCheckedModeBanner: false, 
+), 
+); 
+} 
